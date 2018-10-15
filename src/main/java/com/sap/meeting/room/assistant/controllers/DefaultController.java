7601028eee713bffbc0ac6e9da0575c79d1cc318
Libor Bucek <bucek.libor92@gmail.com>
2018-10-15 07:15:51 +0200

    Initial commit

---
+++ src/main/java/com/sap/meeting/room/assistant/controllers/DefaultController.java
@@ -0,0 +1,16 @@
+package com.sap.meeting.room.assistant.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class DefaultController {
+
+    @GetMapping
+    public String get() {
+
+        return "ok";
+    }
+}
